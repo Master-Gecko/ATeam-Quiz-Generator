776b73ba5581c125e7da4368c89577269face384
Sammy <zopf@wisc.edu>
2019-04-23 00:09:09 -0500

    HashTableADT v1

---
+++ HashTableADT.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ * Filename:   HashTableADT.java
+ * Project:    ATeam Quiz Generator
+ * Course:     cs400
+ * Authors:    Sammy Zopf 
+ * 
+ */
+public interface HashTableADT<K extends Comparable<K>,V> {
+	
+	/**
+	 * Add the key and value to the hash table and increase the number of keys.
+	 * @param key
+	 * @param value
+	 */
+	void insert(K key, V value) ;
+	
+	/**
+	 * Remove the key from the hash table and decrease the number of keys.
+	 * @param key
+	 * @return
+	 */
+	boolean remove(K key) ;
+	
+	/**
+	 * Gets the value paired with the key
+	 * @param key
+	 * @return the value associated with the key
+	 */
+	V get(K key) ;
+	
+	/**
+	 * @return the number of keys in the hash table
+	 */
+	int numKeys() ;
+
+	/**
+	 * @return the load factor threshold
+	 */
+    public double getLoadFactorThreshold() ;
+
+    /**
+     * @return the load factor
+     */
+    public double getLoadFactor() ;
+    
+    /**
+     * @return the capacity of the hash tables
+     */
+    public int getCapacity() ;
+
+}
