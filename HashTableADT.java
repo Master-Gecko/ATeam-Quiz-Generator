889c2983c383f5a7a0ca9dc1a7699e5496c47b42
Sammy <zopf@wisc.edu>
2019-04-23 00:14:43 -0500

    Updated some of the javaDoc

--- HashTableADT.java
+++ HashTableADT.java
@@ -10,21 +10,21 @@ public interface HashTableADT<K extends Comparable<K>,V> {
 	
 	/**
 	 * Add the key and value to the hash table and increase the number of keys.
-	 * @param key
-	 * @param value
+	 * @param key Key to be inserted
+	 * @param value Value to be inserted
 	 */
 	void insert(K key, V value) ;
 	
 	/**
 	 * Remove the key from the hash table and decrease the number of keys.
-	 * @param key
-	 * @return
+	 * @param key Key to be removed
+	 * @return true if key was removed; otherwise false.
 	 */
 	boolean remove(K key) ;
 	
 	/**
 	 * Gets the value paired with the key
-	 * @param key
+	 * @param key Key to be found in the hash table
 	 * @return the value associated with the key
 	 */
 	V get(K key) ;
