82538a6069693b2d489d80f11d9a438dea0b688d
Owen Massey <46950184+bazookag0blin@users.noreply.github.com>
2019-04-30 00:49:44 -0500

    Update FileOut.java

--- application/FileOut.java
+++ application/FileOut.java
@@ -1,10 +1,66 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 /**
  * adding to master branch
+ * 
  * @author odmas
  *
  */
 public class FileOut {
+	private List<Question> questions;
+	private ArrayList<JSONObject> questionArray;
+	
 
+	public FileOut(String filePath, HashTable<Question> hashTable)
+			throws FileNotFoundException, IOException, ParseException {
+//		System.out.println(hashTable.getAllTopics());
+		JSONObject JSONQuestionArray = new JSONObject();
+		questionArray = new ArrayList<JSONObject>();
+		for (String topic : hashTable.getAllTopics()) {
+			try {
+				questions = hashTable.getQuestionsForTopic(topic);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for (Question q : questions) {
+				JSONObject question = new JSONObject();
+				question.put("meta-data", "unused");
+				question.put("questionText", q.getQuestion());
+				question.put("topic", q.getTopic());
+				question.put("image", q.getImagePath());
+				JSONArray answers = new JSONArray();
+				for (Answer a : q.getAnswerList()) {
+					JSONObject curr = new JSONObject();
+					if (a.isCorrect())
+						curr.put("isCorrect", "T");
+					else 
+						curr.put("isCorrect", "F");
+					curr.put("choice", a.toString());
+					answers.add(curr);
+				}
+				question.put("choiceArray", answers);
+				questionArray.add(question);
+			}
+		}
+		JSONQuestionArray.put("questionArray", questionArray);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+		writer.write(JSONQuestionArray.toString());
+		writer.close();
+	}
 }
