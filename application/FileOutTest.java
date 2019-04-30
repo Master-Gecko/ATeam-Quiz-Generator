32d6762789e4c31d7090c82951d126e23335bd54
Owen Massey <46950184+bazookag0blin@users.noreply.github.com>
2019-04-30 00:50:05 -0500

    Update FileOutTest.java

--- application/FileOutTest.java
+++ application/FileOutTest.java
@@ -2,11 +2,15 @@ package application;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
  * adding to master branch
+ * 
  * @author odmas
  *
  */
@@ -18,7 +22,25 @@ class FileOutTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		try {
+			HashTable<Question> hash = new HashTable<Question>();
+			FileIn in = new FileIn("application/questions_003.json", hash);
+			ArrayList<Answer> answers = new ArrayList<Answer>();
+			answers.add(new Answer(false, "answer1"));
+			answers.add(new Answer(false, "answer2"));
+			answers.add(new Answer(true, "answer3"));
+			answers.add(new Answer(false, "answer4"));
+			answers.add(new Answer(false, "answer5"));
+			hash.insertQuestion(new Question("graph", "none", "questionText", "none", answers));
+			FileOut out = new FileOut("application/questions_003_new.json", hash);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("threw exception");
+		}
 	}
 
+	@AfterEach
+	void tearDown() {
+		
+	}
 }
