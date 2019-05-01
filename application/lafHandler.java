349c1a02dddba7d0d32b810b8a77c4bbee037570
Sammy Zopf <zopf@wisc.edu>
2019-05-01 16:53:04 -0500

    Fixed error message on laf file import

--- application/lafHandler.java
+++ application/lafHandler.java
@@ -11,25 +11,25 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+
 /**
  * 
- * Filename:   LoadAdditionalFileScreen.java
- * Project:    ATeam Quiz Generator
- * Course:     cs400 Spring 2019
- * Authors:    Sammy Zopf, Titus Smith
+ * Filename: LoadAdditionalFileScreen.java Project: ATeam Quiz Generator Course: cs400 Spring 2019
+ * Authors: Sammy Zopf, Titus Smith
  * 
  */
 public class lafHandler implements EventHandler<ActionEvent> {
 
-  //Fields
+  // Fields
   private Button b;
   private TextField tf;
   static private File selectedFile;
 
   /**
-  * Constructors
-  * @param b
-  */
+   * Constructors
+   * 
+   * @param b
+   */
   lafHandler(Button b) {
     this.b = b;
   }
@@ -41,37 +41,34 @@ public class lafHandler implements EventHandler<ActionEvent> {
 
   @Override
   public void handle(ActionEvent event) {
-	//Handler for picking a file
-	  //Opens a new window to select a file from
+    // Handler for picking a file
+    // Opens a new window to select a file from
     if (b.getText().equals("Pick a File From System")) {
-    	
+
       FileChooser fileChooser = new FileChooser();
       fileChooser.setTitle("Open Resource File");
       fileChooser.getExtensionFilters().addAll(new ExtensionFilter("JSON Files", "*.json"));
       fileChooser.setInitialDirectory(new File("."));
-      
-      //opens the new window in program's directory and stores the choosen file
-      lafHandler.selectedFile = fileChooser.showOpenDialog(((Node)event.getSource()).getScene().getWindow());
-      	if(selectedFile != null) {
-      		tf.setText(selectedFile.getName());
-      	}
-    }
-    
-    //Handler for importing a file
-    if (b.getText().equals("Import File")) {
+
+      // opens the new window in program's directory and stores the chosen file
+      lafHandler.selectedFile =
+          fileChooser.showOpenDialog(((Node) event.getSource()).getScene().getWindow());
       if (selectedFile != null) {
-        try {
-          FileIn fileIn = new FileIn(selectedFile.getAbsolutePath(), Main.questionTable);
-          selectedFile = null;
-          tf.clear();
-        } catch (Exception e) {
-          tf.setText("Could Not Parse File");
-        }
+        tf.setText(selectedFile.getName());
       }
+    }
+
+    // Handler for importing a file
+    if (b.getText().equals("Import File")) {
       try {
-        FileIn fileIn = new FileIn(tf.getText(), Main.questionTable);
+        if (selectedFile != null) {
+          FileIn fileIn = new FileIn(selectedFile.getAbsolutePath(), Main.questionTable);
+        } else {
+          FileIn fileIn = new FileIn(tf.getText(), Main.questionTable);
+        }
         selectedFile = null;
         tf.clear();
+
       } catch (Exception e) {
         tf.setText("Could Not Parse File");
       }
