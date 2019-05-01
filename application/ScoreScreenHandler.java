19ebdc420f88a29a7d8f5370e16e8c0c71cf89b9
tsmith42 <47675533+tsmith42@users.noreply.github.com>
2019-05-01 18:58:05 -0500

    only one button on scorescreen

--- application/ScoreScreenHandler.java
+++ application/ScoreScreenHandler.java
@@ -1,43 +1,88 @@
 package application;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * 
- * Filename:   QuestionScreenHandler.java
+ * Filename:   ScoreScreen.java
  * Project:    ATeam Quiz Generator
  * Course:     cs400 Spring 2019
  * Authors:    Titus Smith
  * 
+ * Creates a scene for displaying the user's score and a 
+ * few user prompts, returning the scene. 
  */
-public class ScoreScreenHandler implements EventHandler<ActionEvent> {
+public class ScoreScreen extends Scene {
 	
-	Button b;
-	
-	ScoreScreenHandler(Button b) {
-		this.b = b;
+	  //Node fields
+	  private Scene scoreScreen;
+	  private String title;
+	  static Button exitQuiz;
+	  
+	  public ScoreScreen(Parent parent) {
+		  super(parent);
+		   title = "Your Score";
+		    // Create a GridPane
+			GridPane root = new GridPane();
+			
+			//Result message
+			Text result = new Text("You got "+Main.qd.getNumCorrect()+"/"+Main.qd.getNumQuestions());
+			
+			//Exit quiz button
+			exitQuiz = new Button("Return to Menu");
+			exitQuiz.setOnAction(new ScoreScreenHandler(exitQuiz));
+			
+			//Set row and column constraints
+			for (int i = 0; i < 5; i++) {
+		         ColumnConstraints column = new ColumnConstraints(160);
+		         root.getColumnConstraints().add(column);
+		     }
+			for (int i = 0; i < 6; i++) {
+		         RowConstraints row = new RowConstraints(100);
+		         root.getRowConstraints().add(row);
+		     }
+			//Set padding and gaps
+			root.setPadding(new Insets(20, 20, 20, 20));
+			root.setVgap(5);
+			
+			//Format the boxes
+			root.add(result, 2, 0);
+			root.add(exitQuiz, 0, 3);
+			
+			scoreScreen = new Scene(root, 800,600);
+			scoreScreen.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 	}
-
+	
+	
 	/**
-	 * Handler method for buttons pressed
+	 * Getter method for scene
+	 * @return
 	 */
-	@Override
-	public void handle(ActionEvent event) {
-
-		//Switch to OpeningScreen
-		if (b.getText().equals("Return to Menu")) {
-			Group parent = new Group();
-			OpeningScreen os = new OpeningScreen(parent);
-			Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			primaryStage.setScene(os.getScene());
-			primaryStage.setTitle(os.getTitle());
-		}
+	public Scene getScene() {
+		return this.scoreScreen;
 	}
+	
+	/**
+	 * Getter method for title
+	 * @return
+	 */
+	public String getTitle() {
+		return title;
+	}
+	
 }
