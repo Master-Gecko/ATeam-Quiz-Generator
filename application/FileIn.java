13b5fb8d0a954909946e7c708cfb1020c33e11a1
Owen Massey <46950184+bazookag0blin@users.noreply.github.com>
2019-05-02 14:54:07 -0500

    comments

--- application/FileIn.java
+++ application/FileIn.java
@@ -12,12 +12,15 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- * adding to master branch
+ * File in class that takes a string for the input file and hash table and
+ * parses through all the json data to import each question
+ * 
  * @author odmas
  *
  */
 public class FileIn {
 	private ArrayList<Question> questions = new ArrayList<Question>();
+
 	public FileIn(String filePath, HashTable<Question> hash) throws FileNotFoundException, IOException, ParseException {
 		JSONObject jo = (JSONObject) new JSONParser().parse(new FileReader(filePath));
 		JSONArray packages = (JSONArray) jo.get("questionArray"); // finds the start of packages data
@@ -29,24 +32,25 @@ public class FileIn {
 			String image = (String) jsonPackage.get("image");
 			JSONArray choiceArray = (JSONArray) jsonPackage.get("choiceArray");
 //			System.out.println("metadata: " + metadata + " questionText: " + questionText + " topic: " + topic + " image: " + image + " choiceArrayLength: " + choiceArray.size());
-			//TODO: must convert coiceArray to arraylist of answers
+			// TODO: must convert coiceArray to arraylist of answers
 			ArrayList<Answer> answers = new ArrayList<Answer>();
-			for (int j = 0; j < choiceArray.size(); j ++) {
+			for (int j = 0; j < choiceArray.size(); j++) {
 				JSONObject answer = (JSONObject) choiceArray.get(j);
 				Boolean correct = false;
 				if (answer.get("isCorrect").equals("T"))
 					correct = true;
 				String answerText = (String) answer.get("choice");
-				answers.add(new Answer(correct,answerText));
+				answers.add(new Answer(correct, answerText));
 			}
-			questions.add(new Question(topic,metadata, questionText, image, answers));
+			questions.add(new Question(topic, metadata, questionText, image, answers));
 		}
 		for (Question q : questions) {
 			try {
+				//insert each question into the hash table
 				hash.insertQuestion(q);
 			} catch (IllegalNullKeyException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				//e.printStackTrace();
 			}
 		}
 	}
